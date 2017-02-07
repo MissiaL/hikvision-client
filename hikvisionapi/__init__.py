f7fbfd258bf59e961dbc4a5aef0e618983786f13
p.alekseev <petrmissial@gmail.com>
2017-02-07 22:43:54 +0300

    Update version to 0.1.2

--- hikvisionapi/__init__.py
+++ hikvisionapi/__init__.py
@@ -1,7 +1,7 @@
 from hikvisionapi import Client
 
 __title__ = 'hikvisionapi'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Petr Alekseev'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 Petr Alekseev'
