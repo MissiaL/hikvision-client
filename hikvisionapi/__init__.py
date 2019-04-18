173796940953ee9a3b13baf8f7d0f80aa260de51
p.alekseev <petrmissial@gmail.com>
2019-04-18 14:22:52 +0300

    Bump version

--- hikvisionapi/__init__.py
+++ hikvisionapi/__init__.py
@@ -1,7 +1,7 @@
 from .hikvisionapi import Client
 
 __title__ = 'hikvisionapi'
-__version__ = '0.2'
+__version__ = '0.2.1'
 __author__ = 'Petr Alekseev'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Petr Alekseev'
