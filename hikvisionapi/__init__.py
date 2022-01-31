6897444cabec18ea055d6696b74611813b0b3a5c
missial <petrmissial@gmail.com>
2022-01-31 11:37:17 +0300

    Update version

--- hikvisionapi/__init__.py
+++ hikvisionapi/__init__.py
@@ -2,7 +2,7 @@ from .hikvisionapi import AsyncClient
 from .hikvisionapi import Client
 
 __title__ = 'hikvisionapi'
-__version__ = '0.2.1'
+__version__ = '0.3.2'
 __author__ = 'Petr Alekseev'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Petr Alekseev'
